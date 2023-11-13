a26a5fbfd6527abec6d811a33b7c843b6b096333
phapltpc05938 <139949369+phapltpc05938@users.noreply.github.com>
2023-11-13 10:41:20 +0700

    commit lần1
    Chào các cháu

---
+++ QuanLyQuanCafe_DuAn1/src/quanlyquancafe_duan1/QuanLyQuanCafe_DuAn1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quanlyquancafe_duan1;
+
+/**
+ *
+ * @author thanh
+ */
+public class QuanLyQuanCafe_DuAn1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Chào các cháu đến với ah");
+    }
+    
+}
